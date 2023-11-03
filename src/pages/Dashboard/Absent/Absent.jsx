893228c9eb826eba4d5e228ea30624ea78a90c83
Zenny <nhandinhdo291@gmail.com>
2023-11-03 13:30:34 +0700

    fix absent management page

--- src/pages/Dashboard/Absent/Absent.jsx
+++ src/pages/Dashboard/Absent/Absent.jsx
@@ -1,23 +1,22 @@
 import { Button, Col, Modal, Popconfirm, Row, Space, Table, notification, Layout, Tag } from "antd"
 import Sidebar from "../../../components/sidebar/Sidebar"
 import { useEffect, useState } from "react"
-import { getAllAbsent, approveAbsent,rejectAbsent } from "../../../apis/AbsentService"
+import { getAllAbsent, approveAbsent, rejectAbsent } from "../../../apis/AbsentService"
 // import AddModal from "./Modal/AddModal"
 // import EditModal from "./Modal/EditModal"
 
 const Absent = () => {
-        const [absent, setAbsent] = useState([])
-       const [loading, setLoading] = useState(true)
-       const [turnon, setTurnon] = useState(true)
+    const [absent, setAbsent] = useState([])
+    const [loading, setLoading] = useState(true)
     const columns = [
         {
             title: "Avatar",
             dataIndex: ['teacher', 'avatar'],
             render: (avatar) => {
-                if(avatar == "" ||avatar == null ){
+                if (avatar == "" || avatar == null) {
                     avatar = pic;
                 }
-                return(
+                return (
                     <img src={avatar} alt="avatar" style={{ width: '70px', height: '70px' }} />
                 )
             }
@@ -38,140 +37,133 @@ const Absent = () => {
         {
             title: "trạng thái",
             dataIndex: "status",
-            render: (record) => {       
+            render: (record) => {
                 let color;
                 if (record === 'rejected') {
-                  color = 'red';
+                    color = 'red';
                 }
-                else if(record === 'pending'){
+                else if (record === 'pending') {
                     color = 'geekblue';
                 }
-                else if(record === 'approved'){
+                else if (record === 'approved') {
                     color = 'green';
                 }
 
                 return (
-                  <Tag color={color}>
-                    {record.toUpperCase()}
-                  </Tag>
+                    <Tag color={color}>
+                        {record.toUpperCase()}
+                    </Tag>
                 );
-        },
+            },
         },
         {
             title: 'Tác vụ',
             key: 'action',
             align: 'center',
             render: (record) => (
+                // if status is pending, will show 2 buttons else hide 2 buttons
                 <Space size="middle">
+                    {record.status === "pending" ? (<>
+                        <Button onClick={() => {
+                            Approve(record);
+                        }}
+                            style={{ color: "green"}} >Approve</Button>
+                        <Button onClick={() => {
+                            Reject(record);
+                        }}
+                            style={{ color: "red" }}  >Reject</Button>
+                    </>) : (
+                        <>
 
-                    <Button  onClick={() => {
-                        Approve(record);
-                    }}
-                        style={{ color: "green", display: turnon? "inline-block":"none"}} >Approve</Button>
-                    <Button onClick={() => {
-                         Reject(record);
-                    }}
-                        style={{ color: "red"}}  >Reject</Button>
+                        </>
+                    )}
                 </Space>
             ),
         },
     ]
 
-       const getAllAbsents = () => {
-           setLoading(true)
-           getAllAbsent().then((res) => {
+    const getAllAbsents = () => {
+        setLoading(true)
+        getAllAbsent().then((res) => {
             console.log(res.data.data.Absent)
-            const pendingAbsent = res.data.data.Absent.filter(pendingAbsent => {
-                return pendingAbsent.status === "pending"
-            })
-            setAbsent(pendingAbsent)
+            setAbsent(res.data.data.Absent)
             setLoading(false)
-           }).catch((err) => {
-               console.log(err)
-               setLoading(false)
-           })
-       }
+        }).catch((err) => {
+            console.log(err)
+            setLoading(false)
+        })
+    }
 
 
-       const Approve = (record) => {
+    const Approve = (record) => {
         Modal.confirm({
-      title: 'bạn có chắc chắn approve ?',
-      okText: 'Thay đổi',
-      okType: 'danger',
+            title: 'bạn có chắc chắn approve ?',
+            okText: 'Thay đổi',
+            okType: 'danger',
             onOk: () => {
                 setLoading(true);
                 approveAbsent(record._id)
-          .then((res) => {
-                    console.log(res);
-                    setLoading(false);
-                    getAllAbsents();
-                    notification.success({
-              message: 'thay đổi thành công',
-            });
+                    .then((res) => {
+                        console.log(res);
+                        setLoading(false);
+                        getAllAbsents();
+                        notification.success({
+                            message: 'thay đổi thành công',
+                        });
                     })
-          .catch((err) => {
-            console.log(err);
-                });
-      },
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            },
         });
     };
 
     const Reject = (record) => {
         Modal.confirm({
-      title: 'bạn có chắc chắn reject ?',
-      okText: 'Thay đổi',
-      okType: 'danger',
+            title: 'bạn có chắc chắn reject ?',
+            okText: 'Thay đổi',
+            okType: 'danger',
             onOk: () => {
                 setLoading(true);
                 rejectAbsent(record._id)
-          .then((res) => {
-                    console.log(res);
-                    setLoading(false);
-                    getAllAbsents();
-                    notification.success({
-              message: 'thay đổi thành công',
-            });
+                    .then((res) => {
+                        console.log(res);
+                        setLoading(false);
+                        getAllAbsents();
+                        notification.success({
+                            message: 'thay đổi thành công',
+                        });
                     })
-          .catch((err) => {
-            console.log(err);
-                });
-      },
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            },
         });
     };
-        const DisplayNone = () => {
-            if(absent.status === "approved" || absent.status === "rejected" ){
-                setTurnon(false)
-            }
-        }
-    
-       useEffect(() => {
+
+    useEffect(() => {
         getAllAbsents()
-        DisplayNone()
-       }, []);
+    }, []);
 
-       useEffect(() => {
-        DisplayNone()
-       });
+    return (
+        <div>
+            <Row>
+                <Col flex="100px"><Sidebar /></Col>
+                <Col flex="auto">
+                    <Layout
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            minHeight: "100%",
 
-       return (
-           <div>
-               <Row>
-                   <Col flex="100px"><Sidebar /></Col>
-                   <Col flex="auto">
-                       <Layout
-                       style={{
-                           padding: 24,
-                           margin: 0,
-                           minHeight: "100%",
-
-                         }}
-                       >
-                       <Table loading={loading} pagination={{ pageSize: 8 }} columns={columns} dataSource={absent} />
-                       </Layout>
-                   </Col>
-               </Row>
-           </div>
-       )
+                        }}
+                    >
+                        <Table loading={loading} pagination={{ pageSize: 8 }} columns={columns} dataSource={absent} />
+                    </Layout>
+                </Col>
+            </Row>
+        </div>
+    )
 }
 
 export default Absent
