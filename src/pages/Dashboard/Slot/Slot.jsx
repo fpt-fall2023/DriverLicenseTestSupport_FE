272e39a443dfa3b8827a2ab7449ee73b620089b8
Minh Nguyen <morongvp2@gmail.com>
2023-11-06 22:49:52 +0700

    user profile

--- src/pages/Dashboard/Slot/Slot.jsx
+++ src/pages/Dashboard/Slot/Slot.jsx
@@ -1,5 +1,5 @@
 import { Button, Col, Modal, Popconfirm, Row, Space, Table, notification, Layout } from "antd"
-import Sidebar from "../../../components/sidebar/sidebar"
+import Sidebar from "../../../components/sidebar/Sidebar"
 import { useEffect, useState } from "react"
 import { deleteSlot, getSlot } from "../../../apis/SlotService"
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons"
